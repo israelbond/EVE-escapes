a44db23b45d1baa340549d86b219ebdd56dc796f
BrianDavidAllen <33296037+BrianDavidAllen@users.noreply.github.com>
2018-05-23 18:12:41 -0700

    js script for print to table
    js script now prints system_name, system_id, connections.
    formating is still needed.

--- swaggerScripts/load_file.js
+++ swaggerScripts/load_file.js
@@ -1,3 +1,4 @@
+//open json file from relative directory 
 function readTextFile(file, callback) {
     var rawFile = new XMLHttpRequest();
     rawFile.overrideMimeType("application/json");
@@ -9,14 +10,24 @@ function readTextFile(file, callback) {
     }
     rawFile.send(null);
 }
-
+//add data to table using getElementById and layout of json file
 readTextFile("map_connections.json", function(text){
 	    var data = JSON.parse(text);
+	    var table = document.getElementById("system_table");
+	    var row;
+	    var cell0;
+	    var cell1;
+	    var cell2;
 		for (system_id in data.systems){
-			var record = "<tr><td>" + system_id + "</td><td>";
-			for(connection in data.systems[system_id].connections){
-				record +=  connection + "</td><td>";
-			}
-			record +=  "</td></tr>";
+			row = table.insertRow(0);
+			cell0 = row.insertCell(0);
+			cell0.innerHTML = data.systems[system_id].system_name;
+			cell1 = row.insertCell(1);
+			cell1.innerHTML = system_id;
+			for(i = 0; i < data.systems[system_id].connections.length; i++){
+				
+				cell2 = row.insertCell(2);
+				cell2.innerHTML = data.systems[system_id].connections[i];
+		}
 		}
 });
