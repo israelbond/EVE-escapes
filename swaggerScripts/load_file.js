a42e8512dc0e3637bfb24f70979842570f7ef330
israelbond <ibond@pdx.edu>
2018-06-03 13:46:50 -0700

    added stylesheet.css file

--- swaggerScripts/load_file.js
+++ swaggerScripts/load_file.js
@@ -22,13 +22,15 @@ readTextFile("map_connections.json", function(text){
 			row = table.insertRow(0);
 			cell0 = row.insertCell(0);
 			cell0.innerHTML = data.systems[system_id].system_name;
-			cell0.color =
+			cell0.style.backgroundColor = "yellow";
 			cell1 = row.insertCell(1);
 			cell1.innerHTML = system_id;
+			cell1.style.backgroundColor = "deepskyblue"
 			for(i = 0; i < data.systems[system_id].connections.length; i++){
 				
 				cell2 = row.insertCell(2);
 				cell2.innerHTML = data.systems[system_id].connections[i];
+				cell2.style.backgroundColor = "cornflowerblue"
 		}
 		}
 
