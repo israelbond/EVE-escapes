fabe11996e27e35b401d069e7d3f6209a85d40a2
Brian Allen <briallen@little.cs.pdx.edu>
2018-06-10 14:34:42 -0700

    updateDB.py now keeps running updating kills from CCP every hour and adds to kill via zkillboard

--- sqlite/updateDB.py
+++ sqlite/updateDB.py
@@ -1,20 +1,53 @@
 import json
 import sqlite3
 import urllib.request
+import time
+from datetime import datetime
+from datetime import timedelta
 
-#updates sqlite database with kill data from CCP
-#need to run resetDB.py first to reset kills to zero. 
+#updates sqlite database with kill data from CCP and Zkillboard via reisq.zkillboard
+#credit for zkillboard
 
-#open connection to CCP to get kill data
+update_time = datetime.now() + timedelta(hours = 1)
+
 with urllib.request.urlopen('https://esi.tech.ccp.is/latest/universe/system_kills/') as h1:
-    universe_kills = json.loads(h1.read().decode('utf-8'))
-
+	universe_kills = json.loads(h1.read().decode('utf-8'))
 #connect to local .db file
 db = sqlite3.connect('eve_system.db')
 c = db.cursor()
-count = 0;
 #updates kills
 for system in universe_kills:
-    count += 1
-    c.execute('''UPDATE systems SET kills = ? WHERE system_id = ?''', (system['ship_kills'], system['system_id']))                        
-db.commit()
+	c.execute('''UPDATE systems SET kills = ? WHERE system_id = ?''', (system['ship_kills'], system['system_id']))                        
+	db.commit()
+
+while True: 
+	if datetime.now() >= update_time:
+		update_time = datetime.now() + timedelta(hours = 1)
+		print("loop hit!")
+	#open connection to CCP to get kill data
+		with urllib.request.urlopen('https://esi.tech.ccp.is/latest/universe/system_kills/') as h1:
+			universe_kills = json.loads(h1.read().decode('utf-8'))
+	#connect to local .db file
+#		db = sqlite3.connect('eve_system.db')
+#		c = db.cursor()
+	#updates kills
+		for system in universe_kills:
+			c.execute('''UPDATE systems SET kills = ? WHERE system_id = ?''', (system['ship_kills'], system['system_id']))                        
+			db.commit()
+	else:
+#		db = sqlite3.connect('eve_system.db')
+#		c = db.cursor()
+		webUrl = urllib.request.urlopen('https://redisq.zkillboard.com/listen.php')
+		data = webUrl.read() 
+		jData = json.loads(data.decode('utf-8'))
+		if jData['package'] is None:
+			time.sleep(60)
+			print("waiting..")
+		try :
+			jData['package']['killmail']['victim']['character_id']
+			c.execute('''UPDATE systems SET kills = kills + ? WHERE system_id = ?''', ( 1, jData['package']['killmail']['solar_system_id']))
+			db.commit()
+			print("zKill")
+		except:
+			print("issue")
+			continue
---
+++ sqlite/zkill.py
@@ -0,0 +1,32 @@
+import json
+import urllib.request
+import requests
+import time
+#Still need to use time to sort through zkill data. There is more of an hour range in return kill mails. 
+#create object to store zkill data in.
+class Zkill:
+    def __init__(self, zTime, location):
+        self.zTime = zTime
+        self.location = location
+
+#Print killmails to screen with time, system, and character id. Some kills mails don't have a character_id and I'm unsure of why. Will loop until redisq sever sends a "null package" 
+while True:
+	while True:
+		webUrl = urllib.request.urlopen('https://redisq.zkillboard.com/listen.php') 
+		data = webUrl.read()
+		jData = json.loads(data.decode('utf-8'))
+		#print(jData)
+		if jData['package'] is None:
+			time.sleep(15)
+			print("waiting")
+			break
+		zTime = jData['package']['killmail']['killmail_time']
+		#parts = zTime.split('T')
+		#time = parts[1]
+		#time = time[:-1]
+		#Zkill.time = time
+		Zkill.location = jData['package']['killmail']['solar_system_id']
+		try:
+			print(zTime, Zkill.location, jData['package']['killmail']['victim']['character_id'] )
+		except:
+			print(zTime, Zkill.location )
